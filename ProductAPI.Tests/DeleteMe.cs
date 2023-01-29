5b4161cdcfafcbf6f7b1478a30d8a08f8f98ac77
BoxingBox <c.tzoulias@gmail.com>
2023-01-29 17:38:07 +0200

    Introduced Acceptance tests
    - Added acceptance test

---
+++ ProductAPI.Tests/DeleteMe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Tests
+{
+    internal class DeleteMe   //Ensures tests projects and app are discoverable and tests are running
+    {
+        public void ShouldBetrue() => Assert.True(condition: true);
+
+    }
+}

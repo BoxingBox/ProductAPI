a1910aecd07caca2843484a75b38c31df7765e43
BoxingBox <c.tzoulias@gmail.com>
2023-01-31 11:44:43 +0200

    New test project
    Added a new Tests.Acceptance project with ApiTestCollection and ProductApiBroker classes

---
+++ ProductAPI.Tests.Acceptance/Brokers/ProductApiBroker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using RESTFulSense.Clients;
+
+namespace ProductAPI.Tests.Acceptance.Brokers
+{
+    public class ProductApiBroker
+    {
+        private readonly WebApplicationFactory<Program>? _webApplicationFactory;
+        private readonly HttpClient? _httpClient;
+        private readonly IRESTFulApiFactoryClient? _apiFactoryClient;
+
+        public ProductApiBroker()
+        {
+            this._webApplicationFactory = new WebApplicationFactory<Program>();
+            this._httpClient = this._webApplicationFactory.CreateClient();
+            this._apiFactoryClient = new RESTFulApiFactoryClient(this._httpClient);
+        }
+    }
+}
